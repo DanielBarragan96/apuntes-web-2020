d9dcbe90b7b230f0ae51237372585aae28eea3e4
DanielBarragan96 <ie702554@iteso.mx>
2020-10-22 17:04:09 -0500

    s20 events, creation of simple html & js calculator

---
+++ Javascript/s20 eventos/index.js
@@ -0,0 +1,45 @@
+let calc = document.getElementById('calculadora');
+let num1 = undefined;
+let num2 = undefined;
+let op = undefined;
+let res = undefined;
+let afet_equal = false;
+
+calc.addEventListener('click', (event) => {
+    let key = event.target.innerHTML;
+    if (key == "+") {
+        op = key;
+        num1 = document.getElementById("result").value;
+        document.getElementById("result").value = "";
+    } else if (key == "-") {
+        op = key;
+        num1 = document.getElementById("result").value;
+        document.getElementById("result").value = "";
+    } else if (key == "=") {
+        num2 = document.getElementById("result").value;
+        if (num1 !== undefined && num2 !== undefined) {
+            num1 = parseInt(num1);
+            num2 = parseInt(num2);
+            if(op == "+") res = num1 + num2;
+            else if(op == "-") res = num1 - num2;
+            document.getElementById("result").value = res;
+            afet_equal = true;
+            // console.log(num1 + op + num2 + key + res);
+            op = undefined;
+            num1 = undefined;
+            num2 = undefined;
+            res = undefined;
+        } else {
+            document.getElementById("result").value = "";
+            op = undefined;
+            num1 = undefined;
+            num2 = undefined;  
+            res = undefined;          
+        }
+    } else {
+        if(afet_equal) {
+            document.getElementById("result").value = "";
+        }
+        document.getElementById("result").value += key;
+    }
+})
