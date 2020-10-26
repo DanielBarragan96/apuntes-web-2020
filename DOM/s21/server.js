560893f864925dbdc19dee8c37b1e9ac13064208
DanielBarragan96 <ie702554@iteso.mx>
2020-10-26 08:30:47 -0600

    s20 code, example create ibm bluemix webpage with nodejs, and using cookies

---
+++ DOM/s21/server.js
@@ -0,0 +1,10 @@
+'use strict';
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.use(express.static(__dirname +'/public'));
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+})
